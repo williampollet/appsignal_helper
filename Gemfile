8f16e9d37e9d6067de28a79ae0c3f0114196f798
William Pollet <william.pollet@kisskissbankbank.com>
2018-05-29 16:19:39 +0200

    adding retrieve parameters

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'faraday'
+gem 'rubocop'
+gem 'json'
+gem 'dotenv'
---
+++ loader.rb
@@ -0,0 +1,5 @@
+require 'json'
+require 'faraday'
+require 'dotenv/load'
+require './appsignal_api.rb'
+require './retrieve_parameters.rb'
